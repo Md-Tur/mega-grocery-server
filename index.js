de28128692c7e6f01f7d2607a3921f1b7da27970
Md-Tur <turibnmostafiz@gmail.com>
2022-05-08 03:54:06 +0600

    fetch data using api

--- index.js
+++ index.js
@@ -9,7 +9,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ysesw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+}); */
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1lzp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('megaGrocery').collection('items');
+
+        //all data get
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
